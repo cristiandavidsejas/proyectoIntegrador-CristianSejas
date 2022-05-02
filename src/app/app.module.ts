4ddef5d8340de19017d53abf35118fcb2aa58c38
cristiandavidsejas <davidcris.sejas@gmail.com>
2022-05-02 04:29:37 -0300

    se creo el componente acerca-de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAndRedesSocialesComponent } from './components/logo-and-redes-sociales/logo-and-redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAndRedesSocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
