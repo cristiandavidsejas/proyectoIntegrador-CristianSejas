c94a806eac260566a3ed231c15ad30c95a1cd99d
cristiandavidsejas <davidcris.sejas@gmail.com>
2022-05-02 02:57:31 -0300

    se creo el componente banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAndRedesSocialesComponent } from './components/logo-and-redes-sociales/logo-and-redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAndRedesSocialesComponent
+    LogoAndRedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
