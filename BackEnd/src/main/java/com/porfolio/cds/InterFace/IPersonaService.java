beb3bbc58079264b67ca36df710c4878f6e8aeca
cristiandavidsejas <davidcris.sejas@gmail.com>
2022-06-02 02:58:26 -0300

    codificacion en  Java-NetBeans separando por capas

---
+++ BackEnd/src/main/java/com/porfolio/cds/InterFace/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.porfolio.cds.InterFace;
+
+import com.porfolio.cds.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+//traer una lista de personas 
+    public List<Persona>getPersona();
+    
+    //guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //buscar una persona por ID
+    public Persona findPersona(Long Id);
+}
---
+++ BackEnd/src/main/java/com/porfolio/cds/Repository/IPersonaRepository.java
@@ -0,0 +1,13 @@
+
+package com.porfolio.cds.Repository;
+
+import com.porfolio.cds.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+    
+}
